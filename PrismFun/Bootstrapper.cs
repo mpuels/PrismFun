59df66345b383234d7d7ea11667976aec5c5f03c
mpuels <mpuels@gmail.com>
2016-05-31 01:06:03 +0200

    Bootstrapper hinzugefügt.

---
+++ PrismFun/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Unity;
+using Microsoft.Practices.Unity;
+using System.Windows;
+
+namespace PrismFun
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>(new ResolverOverride[] { });
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
